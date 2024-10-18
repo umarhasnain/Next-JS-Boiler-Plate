2c81a1b51074aef8fc6930537bc4fddf5884afd8
Umar Hasnain <umarhasnain045@gmail.com>
2024-10-18 04:30:22 -0700

    fix: Line added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-3xl">Hello Next JS!</h1>
-      {/* <h1 className="bg-black text-white w-[300px] h-[50px] text-3xl text-center p-2">
-        Hello Next JS.
-      </h1> */}
+      <h1 className="text-3xl">Hello World!</h1>
+      <h1 className="bg-black text-white w-[300px] h-[50px] text-3xl text-center p-2">
+        Hello Next js With TypeScript.
+      </h1>
     </div>
   )
 }
