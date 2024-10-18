989c806414e7c878255399d4fbe7ecae078fb984
Umar Hasnain <umarhasnain045@gmail.com>
2024-10-18 03:47:43 -0700

    Your commit message

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,26 @@
-import Module from 'module'
+import module from 'module'
 
-Module.exports = {
-  extends: ['@commitlint/config-conventional'],
+module.exports = {
+  extends: ['@commitlint/cli', '@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [0, 'always', 100],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+      ],
+    ],
+    'subject-case': [2, 'always', 'sentence-case'],
+  },
 }
